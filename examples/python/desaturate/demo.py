a12631822e55a8b6ae61998d6d2a45e17d8c80a5
Rich Wareham <richwareham@gmail.com>
2008-06-23 20:13:52 +0100

    Added simple desaturate demo.

---
+++ examples/python/desaturate/demo.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+# Import the Python OpenGL stuff
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+
+#==============================================================================
+# Use sys to find the script path and hence construct a path in which to 
+# search for the Firtree bindings.
+
+import sys
+import os
+import gc
+
+# Find the script path.
+scriptDir = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+# Walk down three subdirs...
+rootDir = os.path.dirname(os.path.dirname(os.path.dirname(scriptDir)))
+
+# Add path to the python bindings
+importDir = os.path.join(rootDir, 'bindings', 'python')
+
+# Create path to the example images
+imageDir = os.path.join(rootDir, 'artwork')
+
+# Append this path to our search path & import the firtree bindings.
+sys.path.append(importDir)
+
+import Firtree
+
+class FirtreeScene:
+
+    def init (self):
+        # Set background colour & disable depth testing
+        glClearColor(0, 0, 0, 1)
+        glDisable(GL_DEPTH_TEST)
+
+        # Setup a desaturate kernel.
+        desaturateKernel = Firtree.CreateKernel('''
+        kernel vec4 desatKernel(sampler source)
+        {
+            vec4 sourceColour = sample(source, samplerCoord(source));
+            vec4 rgb2int = vec4(0.299, 0.587, 0.114, 0.0);
+            float intensity = dot(rgb2int, sourceColour); 
+            return vec4(intensity, intensity, intensity, sourceColour.a);
+        }
+        ''')
+        compositeImage = Firtree.Image.CreateFromKernel(desaturateKernel)
+
+        # Load the firtree image.
+        firtreeImage = Firtree.Image.CreateFromFile(
+            os.path.join(imageDir, 'firtree-192x192.png'))
+
+        # Form a translated and scaled firtree image.
+        firtreeTransform = Firtree.AffineTransform.Translation(-96, -96)
+        firtreeTransform.ScaleBy(2,2)
+        firtreeTransform.TranslateBy(400,300)
+        firtreeTransImage = Firtree.Image.CreateFromImageWithTransform(
+            firtreeImage, firtreeTransform)
+
+        # Wire the lena and firtree images into the kernel.
+        desaturateKernel.SetValueForKey(
+            Firtree.CreateSampler(firtreeTransImage), 'source')
+
+        # Create a rendering context.
+        sampler = Firtree.CreateSampler(compositeImage)
+        self.renderContext = Firtree.CreateRenderingContext(sampler)
+
+    def display (self, width, height):
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        # Render the composited image into the framebuffer.
+        Firtree.RenderAtPoint(self.renderContext, 
+            Firtree.Point2D(0,0),
+            Firtree.Rect2D(0,0,width,height))
+
+    def clear_up (self):
+        print('Clearing up...')
+
+        # Release the rendering context.
+        Firtree.ReleaseRenderingContext(self.renderContext)
+
+        # Sanity check to make sure that there are no objects left
+        # dangling.
+        gc.collect()
+        globalObCount = Firtree.ReferenceCounted.GetGlobalObjectCount()
+        print('Number of objects still allocated (should be zero): %i' 
+            % globalObCount)
+
+    def reshape (self, width, height):
+        # Resize the OpenGL viewport
+        glViewport(0, 0, width, height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0.0, width, 0.0, height, -1.0, 1.0)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        
+    def keypress(self, key):
+        if(key == 'q'):
+            print('Quitting...')
+            quit()
+
+def reshape(w, h):
+    global winWidth, winHeight
+
+    winWidth = w
+    winHeight = h
+    glscene.reshape(winWidth, winHeight)
+
+def quit():
+    glscene.clear_up()
+    sys.exit()
+
+def display():
+    global shouldInit, shouldQuit
+
+    if(shouldInit):
+        glscene.init()
+        shouldInit = False
+
+    glscene.display(winWidth, winHeight)
+    glutSwapBuffers()
+
+def keypress(k, x, y):
+    glscene.keypress(k)
+
+def timer(val):
+    glutSetWindow(window)
+    glutPostRedisplay()
+    glutTimerFunc(frameDelay, timer, val)
+
+if __name__ == '__main__':
+    glscene = FirtreeScene()
+
+    shouldInit = True
+    winWidth = winHeight = 0
+    frameDelay = 1000/60
+
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+    glutInitWindowSize(800, 600)
+    window = glutCreateWindow("Firtree Demo")
+
+    glutDisplayFunc(display)
+    glutReshapeFunc(reshape)
+    glutKeyboardFunc(keypress)
+    
+    # glutTimerFunc(frameDelay, timer, 0)
+
+    glutMainLoop()
+
+# vim:sw=4:ts=4:et:autoindent

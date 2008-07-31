3c5fa756c508199568c925d17a1ce3f3e67dae7c
Rich Wareham <richwareham@gmail.com>
2008-07-31 16:28:54 +0100

    Added more complex image compositing.

---
+++ testing/tests/test0003.py
@@ -0,0 +1,34 @@
+from TestModule import *
+from Firtree import *
+
+class Test (TestModule):
+    def name(self):
+        return 'complex image compositing'
+
+    def run_test(self, context, renderer, helper):
+        fog_orig = helper.load_image('fog.png')
+        lena_orig = helper.load_image('lena.png')
+
+        trans1 = AffineTransform.Scaling(1, 1.5)
+        fog = Image.CreateFromImageWithTransform(fog_orig, trans1)
+
+        trans2 = AffineTransform.Translation(-256,-256)
+        trans2.RotateByDegrees(-10)
+        lena = Image.CreateFromImageWithTransform(lena_orig, trans2)
+
+        kernel = Kernel.CreateFromSource('''
+        kernel vec4 testKernel(sampler a, sampler b) 
+        {
+            vec4 aCol = sample(a, samplerCoord(a));
+            vec4 bCol = sample(b, samplerCoord(b));
+            return aCol + bCol * (1.0 - aCol.a);
+        }
+        ''')
+        outimage = Image.CreateFromKernel(kernel);
+
+        kernel.SetValueForKey(fog, 'a')
+        kernel.SetValueForKey(lena, 'b')
+
+        helper.write_test_output(outimage)
+
+# vim:sw=4:ts=4:et

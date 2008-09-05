a938c875f47aa8413ac35491781b122bf080fb3f
Rich Wareham <richwareham@gmail.com>
2008-09-05 14:08:37 +0100

    * Modified page curl demo to be a slide show app, added more comments.
    * Added some more artwork for examples.

--- examples/python/pagecurl/demo.py
+++ examples/python/pagecurl/demo.py
@@ -35,50 +35,76 @@ import math
 class PageCurlTransition:
     def __init__(self):
         self._output = None
-        self._start_image = None
-        self._end_image = None
+        self._front_image = None
+        self._back_image = None
 
         kernelSource = '''
+          vec4 atop(vec4 a, vec4 b)
+          {
+            return a + (1.0-a.a)*b;
+          }
+
           kernel vec4 transitionKernel(sampler front, sampler back,
                                        vec2 diagonal, float offset,
                                        float radius)
           {
-            float onDiagonalCoord = dot(destCoord(), diagonal);
-            float offDiagonalCoord = dot(destCoord(), diagonal.yx * vec2(-1,1));
+            // Find the 'background' colour
+            vec4 backColour = sample(back, samplerCoord(back));
 
+            // Firstly we want to calculate the 'along diagonal' 
+            // co-ordinate.
+            float onDiagonalCoord = dot(destCoord(), diagonal);
+
+            // We offset this co-ordinate to advance the animation.
             onDiagonalCoord -= offset;
 
-            vec4 untouchedFront = sample(front, samplerCoord(front));
-            untouchedFront *= step(onDiagonalCoord, 0.0);
+            // Calculate masks for regions A and E. These masks are
+            // 1.0 in the corresponding regions and 0.0 outside.
+            float aMask = step(onDiagonalCoord, 0.0);
+            float eMask = step(radius, onDiagonalCoord);
 
-            float cosThetaB = onDiagonalCoord / radius;
-            float thetaB = acos(cosThetaB);
-            float thetaA = 0.5 * 3.14159 - thetaB;
-            float upturnOffset = radius * thetaA - onDiagonalCoord;
-            float downturnOffset = radius * (thetaB + 0.5*3.14159) - onDiagonalCoord;
+            // We calculate the region A colour first
+            vec4 aColour = sample(front, samplerCoord(front)) * aMask;
 
-            vec4 upturnColour = sample(front, samplerTransform(front,
-                destCoord() + upturnOffset*diagonal));
-            vec4 downturnColour = sample(front, samplerTransform(front,
-                destCoord() + downturnOffset*diagonal));
-            downturnColour *= vec4(0.5, 0.5, 0.5, 1.0);
+            // Calculate the 'backside' colour, or region E
+            float eOffset = - 2.0 * onDiagonalCoord + (3.14159 * radius);
+            vec4 eColour = sample(front, samplerTransform(front,
+                destCoord() + eOffset * diagonal));
 
-            float backsideOffset = - 2.0 * onDiagonalCoord + (3.14159 * radius);
-            vec4 backsideColour = sample(front, samplerTransform(front,
-                destCoord() + backsideOffset*diagonal));
-            backsideColour *= step(cosThetaB, 0.0);
-            backsideColour *= vec4(0.5, 0.5, 0.5, 1.0);
+            // Darken the back side.
+            eColour *= vec4(0.7, 0.7, 0.7, 1.0) * aMask;
 
-            float mask = step(0.0, cosThetaB) * step(cosThetaB, 1.0);
-            upturnColour *= mask;
-            downturnColour *= mask;
+            // Calculate a combined colour for this region
+            vec4 flatColour = atop(eColour, aColour);
 
-            vec4 turnColour = downturnColour + (1.0 - downturnColour.a) * upturnColour;
+            // Now we need to be a bit clever to calculate the curved areas.
+            
+            // Calculate a mask for the curved area
+            float curveMask = (1.0 - aMask) * (1.0 - eMask);
 
-            vec4 frontCol = (1.0-backsideColour.a)*(untouchedFront + turnColour) + backsideColour;
-            vec4 backCol = sample(back, samplerCoord(back));
+            // Calculate the angles for both areas
+            float thetaC = acos(onDiagonalCoord/radius);
+            float thetaB = (0.5*3.14159) - thetaC;
 
-            return (1.0-frontCol.a)*backCol + frontCol;
+            // Given these angles, calculate the offsets for each
+            float bOffset = radius*thetaB - onDiagonalCoord;
+            float cOffset = radius*(thetaC + 0.5*3.14159) - onDiagonalCoord;
+
+            // From the offsets, calculate the region colours.
+            vec4 bColour = sample(front, samplerTransform(front,
+                destCoord() + bOffset * diagonal)) * curveMask;
+            vec4 cColour = sample(front, samplerTransform(front,
+                destCoord() + cOffset * diagonal)) * curveMask;
+
+            // Darken the back side.
+            cColour *= vec4(0.7, 0.7, 0.7, 1.0);
+
+            // Calculate the final curve colour.
+            vec4 curveColour = atop(cColour, bColour);
+
+            vec4 frontColour = atop(curveColour, flatColour);
+
+            return atop(frontColour, backColour);
           }
         '''
 
@@ -88,23 +114,34 @@ class PageCurlTransition:
         self._transitionKernel.SetValueForKey(0.707, 0.707, 'diagonal')
         self._transitionKernel.SetValueForKey(0.0, 'offset')
 
-        self._radius = 40.0
-        self._transitionKernel.SetValueForKey(self._radius, 'radius')
+        self.set_radius(40.0)
 
+    def front_image(self):
+        return self._front_image
 
-    def set_start_image(self, im):
-        self._start_image = im
-        self._transitionKernel.SetValueForKey(self._start_image, 'front')
+    def back_image(self):
+        return self._back_image
 
-    def set_end_image(self, im):
-        self._end_image = im
-        self._transitionKernel.SetValueForKey(self._end_image, 'back')
+    def radius(self):
+        return self._radius
+
+    def set_radius(self, radius):
+        self._radius = radius
+        self._transitionKernel.SetValueForKey(radius, 'radius')
+
+    def set_front_image(self, im):
+        self._front_image = im
+        self._transitionKernel.SetValueForKey(self._front_image, 'front')
+
+    def set_back_image(self, im):
+        self._back_image = im
+        self._transitionKernel.SetValueForKey(self._back_image, 'back')
 
     def set_progress(self, p):
-        
         extent = self.get_output().GetExtent()
         diagonal = math.sqrt(extent.Size.Width*extent.Size.Width + 
             extent.Size.Height*extent.Size.Height)
+        p = 1.0 - p
         self._transitionKernel.SetValueForKey((diagonal + self._radius) * p - self._radius, 'offset')
         self._transitionKernel.SetValueForKey(extent.Size.Width/diagonal,
             extent.Size.Height/diagonal, 'diagonal')
@@ -120,13 +157,27 @@ class FirtreeScene:
         # Create a rendering context.
         self.context = GLRenderer.Create()
 
-        front = Image.CreateFromFile(os.path.join(imageDir,'tariffa.jpg'))
-        back = Image.CreateFromFile(os.path.join(imageDir,'statues.jpg'))
+        self._image_idx = -1
+        self._image_files = [ 'bricks.jpg', 'tariffa.jpg', 'statues.jpg', 
+            'painting.jpg', 'bigben.jpg' ]
+
+        front = self.next_image()
+        back = self.next_image()
 
         self.transition = PageCurlTransition()
-        self.transition.set_start_image(front)
-        self.transition.set_end_image(back)
+        self.transition.set_front_image(front)
+        self.transition.set_back_image(back)
         self.transition.set_progress(0.0)
+        self.transition.set_radius(100.0)
+
+        self._trans_start_time = None
+
+    def next_image(self):
+        self._image_idx += 1
+        if(self._image_idx >= len(self._image_files)):
+            self._image_idx = 0
+
+        return Image.CreateFromFile(os.path.join(imageDir, self._image_files[self._image_idx]))
 
     def display (self, width, height):
         glClearColor(0,0,0,1)
@@ -135,12 +186,23 @@ class FirtreeScene:
         if(self.transition == None):
             pass
 
-        t = float(glutGet(GLUT_ELAPSED_TIME)) / 1000.0
-        self.transition.set_progress(0.5 * (1.0 + math.sin(1.0 * t)))
-        #self.transition.set_progress(0.5)
+        if(self._trans_start_time == None):
+            self._trans_start_time = float(glutGet(GLUT_ELAPSED_TIME)) / 1000.0
+
+        now = float(glutGet(GLUT_ELAPSED_TIME)) / 1000.0
+        delta = now - self._trans_start_time
+        pos = 0.5 * delta
+
+        self.transition.set_progress(max(0.0, min(1.0, pos)))
         
         # Render the composited image into the framebuffer.
         self.context.RenderWithOrigin(self.transition.get_output(), Point2D(0,0))
+
+        if(pos > 1.0):
+            self._trans_start_time = now + 1.0
+            old_back_image = self.transition.back_image()
+            self.transition.set_back_image(self.next_image())
+            self.transition.set_front_image(old_back_image)
 
     def clear_up (self):
         print('Clearing up...')
@@ -158,11 +220,6 @@ class FirtreeScene:
     def reshape (self, width, height):
         # Resize the OpenGL viewport
         glViewport(0, 0, width, height)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(0.0, width, 0.0, height, -1.0, 1.0)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
         
     def keypress(self, key):
         if(key == 'q'):

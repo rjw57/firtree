6d180def784533c4c07d55386442e0a7f306b2f1
Rich Wareham <richwareham@gmail.com>
2008-11-14 18:34:18 +0000

    Add LLVM_{MAJOR,MINOR} variables to report LLVM version.

--- cmake/FindLLVM.cmake
+++ cmake/FindLLVM.cmake
@@ -1,6 +1,9 @@
 # - Find LLVM
 # This module finds an installed LLVM. It sets the following variables:
 #  LLVM_FOUND - set to true if LLVM is found
+#  LLVM_VERSION - set to the LLVM version.
+#  LLVM_MAJOR - set to the LLVM major version number.
+#  LLVM_MINOR - set to the LLVM minor version number.
 #  LLVM_CONFIG_EXECUTABLE - the path to the llvm-config executable
 #  LLVM_INCLUDE_DIR - where to find the LLVM headers.
 #  LLVM_LIBRARY_DIR - the LLVM library directory
@@ -20,13 +23,18 @@ MACRO(LLVM_RUN_CONFIG arg outvar)
 ENDMACRO(LLVM_RUN_CONFIG)
 
 IF(LLVM_CONFIG_EXECUTABLE)
+  LLVM_RUN_CONFIG("--version" LLVM_VERSION)
   LLVM_RUN_CONFIG("--libdir" LLVM_LIBRARY_DIR)
   LLVM_RUN_CONFIG("--includedir" LLVM_INCLUDE_DIR)
   LLVM_RUN_CONFIG("--libs" LLVM_LIBRARIES)
+  STRING(REGEX REPLACE "[ \n\t]+" "" LLVM_VERSION ${LLVM_VERSION})
   STRING(REGEX REPLACE "[ \n\t]+" " " LLVM_LIBRARY_DIR ${LLVM_LIBRARY_DIR})
   STRING(REGEX REPLACE "[ \n\t]+" " " LLVM_INCLUDE_DIR ${LLVM_INCLUDE_DIR})
   STRING(REGEX REPLACE "[ \n\t]+" " " LLVM_LIBRARIES ${LLVM_LIBRARIES})
   SEPARATE_ARGUMENTS(LLVM_LIBRARIES)
+
+  STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)" "\\1" LLVM_MAJOR ${LLVM_VERSION})
+  STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)" "\\2" LLVM_MINOR ${LLVM_VERSION})
 ENDIF(LLVM_CONFIG_EXECUTABLE)
 
 # Assume LLVM is found unless anything indicates otherwise

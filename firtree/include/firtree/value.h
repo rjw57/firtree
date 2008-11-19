aab9f291cf7bc494edc4a7ba4e9198929dcad9e7
Rich Wareham <richwareham@gmail.com>
2008-11-19 13:27:45 +0000

    Document members of Firtree::Value class.

--- firtree/include/firtree/value.h
+++ firtree/include/firtree/value.h
@@ -36,35 +36,90 @@ class Value : public ReferenceCounted
         virtual ~Value();
 
     public:
+        /// Create an empty value. The value has invalid type until
+        /// it is assigned.
         static Value* Create();
+
+        /// Create a value with a specified floating point value.
         static Value* CreateFloatValue(float v);
+
+        /// Create a value with a specified integer value.
         static Value* CreateIntValue(int v);
+
+        /// Create a value with a specified boolean value value.
         static Value* CreateBoolValue(bool v);
+
+        /// Create a 2D vector with specified components.
         static Value* CreateVectorValue(float x, float y);
+
+        /// Create a 3D vector with specified components.
         static Value* CreateVectorValue(float x, float y, float z);
+
+        /// Create a 4D vector with specified components.
         static Value* CreateVectorValue(float x, float y, float z, float w);
+
+        /// Create a nD vector with specified components. 'v' points to
+        /// an array of component values and 'num_components' gives
+        /// its length. If num_components is outside of the range (2,4), 
+        /// an error is thrown.
         static Value* CreateVectorValue(float* v, int num_components);
 
+        /// Return the type of the value.
         inline KernelTypeSpecifier GetType() const { return m_Type; }
 
+        /// Assign a specified floating point value.
         void SetFloatValue(float v);
+
+        /// Assign a specified integer value.
         void SetIntValue(int v);
+
+        /// Assign a specified boolean value.
         void SetBoolValue(bool v);
 
+        /// Get the floating point value. If the value is a vector, this
+        /// is the first component. If the value is integer, this is
+        /// the integer cast to a float. If the value is boolean, this
+        /// is 1.0 if true, 0.0 if false.
         inline float GetFloatValue() const {
             return m_FloatingPointComponents[0]; 
         }
+
+        /// Get the integer value. If the value is a vector, this is the
+        /// first component cast to an integer. If the value is a float, 
+        /// this is the floating point value cast to an integer. If the value
+        /// is a boolean, this is 1 for true and 0 for false.
         inline int GetIntValue() const { return m_IntegerValue; }
+
+        /// Get the boolean value. Equivalent to (GetIntValue() != 0).
         inline bool GetBoolValue() const { return m_IntegerValue != 0; }
 
+        /// Assign a 2D vector with given components to the value.
         void SetVectorValue(float x, float y);
+
+        /// Assign a 3D vector with given components to the value.
         void SetVectorValue(float x, float y, float z);
+
+        /// Assign a 4D vector with given components to the value.
         void SetVectorValue(float x, float y, float z, float w);
+
+        /// Assign the value from an array of vector components. 
+        /// 'num_components' must be in the range (2,4) or an error is
+        /// thrown.
         void SetVectorValue(float* v, int num_components);
 
+        /// Return the vector component values via the passed pointers.
+        /// If pz and/or pw are NULL, the z and/or w components are not
+        /// returned.
         void GetVectorValue(float* px, float* py,
                 float* pz=NULL, float* pw=NULL) const;
+
+        /// Write a copy of the vector into the array pointed to by
+        /// dest. No more than 'expected_components' will be written
+        /// and an error is thrown if 'expected_components' is 
+        /// outside of the range (2,4).
         void GetVectorValue(float* dest, int expected_components) const;
+
+        /// Return a pointer to the internal array of vector components.
         const float* GetVectorValue(uint32_t index) const;
 
     private:

01c6a4696d368e7f4405ec1b8d98844632c02a14
Rich Wareham <richwareham@gmail.com>
2009-06-05 00:35:02 +0100

    Document implemented methods of FirtreeAffineTransform.

--- firtree/firtree-affine-transform.h
+++ firtree/firtree-affine-transform.h
@@ -24,6 +24,14 @@
 
 #include "firtree-vector.h"
 
+/**
+ * SECTION:firtree-affine-transform
+ * @short_description: 2D affine transform support.
+ * @include: firtree/firtree-affine-transform.h
+ *
+ * A FirtreeAffineTransform encapsulates a 2D affine transform.
+ */
+
 G_BEGIN_DECLS
 
 #define FIRTREE_TYPE_AFFINE_TRANSFORM firtree_affine_transform_get_type()
@@ -43,6 +51,20 @@ G_BEGIN_DECLS
 #define FIRTREE_AFFINE_TRANSFORM_GET_CLASS(obj) \
     (G_TYPE_INSTANCE_GET_CLASS ((obj), FIRTREE_TYPE_AFFINE_TRANSFORM, FirtreeAffineTransformClass))
 
+/**
+ * FirtreeAffineTransform:
+ * @m11: The (1,1) element of the transform matrix.
+ * @m12: The (1,2) element of the transform matrix.
+ * @m21: The (2,1) element of the transform matrix.
+ * @m22: The (2,2) element of the transform matrix.
+ * @tx: The translation along x.
+ * @ty: The translation along y.
+ *
+ * A FirtreeAffineTransform encapsulates a 2D affine transform. An affine
+ * transform is one of the form
+ *
+ * (x, y) -> (m11*x + m12*y + tx, m21*x + m22*y + ty).
+ */
 typedef struct {
     GObject parent;
     float m11, m12;
@@ -56,23 +78,71 @@ typedef struct {
 
 GType firtree_affine_transform_get_type (void);
 
+/**
+ * firtree_affine_transform_new:
+ *
+ * Create a new affine transform representing the identity
+ * transform (x,y) -> (x,y).
+ *
+ * Returns: A newly instantiated FirtreeAffineTransform.
+ */
 FirtreeAffineTransform* 
 firtree_affine_transform_new (void);
 
+/**
+ * firtree_affine_transform_clone:
+ * @self: An affine transform.
+ *
+ * Create a new affine transform and copy the value of @self into it.
+ * Return the new transform.
+ *
+ * Returns: A newly instantiated FirtreeAffineTransform.
+ */
 FirtreeAffineTransform* 
 firtree_affine_transform_clone (FirtreeAffineTransform* self);
 
+/**
+ * firtree_affine_transform_is_identity:
+ * @self: An affine transform.
+ *
+ * Determine if @self represents the identity transform.
+ *
+ * Returns: TRUE is @self is an identity transform, FALSE otherwise.
+ */
 gboolean
 firtree_affine_transform_is_identity (FirtreeAffineTransform* self);
 
+/**
+ * firtree_affine_transform_append_transform:
+ * @self: An affine transform.
+ * @trans: An affine transform.
+ *
+ * Modify @self so that it represents the transform @self followed by @trans.
+ */
 void
 firtree_affine_transform_append_transform (FirtreeAffineTransform* self,
         FirtreeAffineTransform* trans);
 
+/**
+ * firtree_affine_transform_append_transform:
+ * @self: An affine transform.
+ * @trans: An affine transform.
+ *
+ * Modify @self so that it represents the transform @trans followed by @self.
+ */
 void
 firtree_affine_transform_prepend_transform (FirtreeAffineTransform* self,
         FirtreeAffineTransform* trans);
 
+/**
+ * firtree_affine_transform_invert:
+ * @self: An affine transform.
+ *
+ * Is @self is an invertable transform, modify it so that it becomes its
+ * inverse.
+ *
+ * Returns: TRUE if @self is invertable, FALSE otherwise.
+ */
 gboolean
 firtree_affine_transform_invert (FirtreeAffineTransform* self);
 
@@ -119,14 +189,47 @@ void
 firtree_affine_transform_set_translation_by (FirtreeAffineTransform* self, 
         float tx, float ty);
 
+/**
+ * firtree_affine_transform_set_elements:
+ * @self: An affine transform.
+ * @m11: The (1,1) element of the transform.
+ * @m12: The (1,2) element of the transform.
+ * @m21: The (2,1) element of the transform.
+ * @m22: The (2,2) element of the transform.
+ * @tx: The translation along x.
+ * @ty: The translation along y.
+ *
+ * Set the transform @self so that it represents the transform
+ *
+ * (x,y) -> ( @m11 * x + @m12 * y + @tx , @m21 * x + @m22 * y + @ty )
+ */
 void
 firtree_affine_transform_set_elements (FirtreeAffineTransform* self, 
         float m11, float m12, float m21, float m22, float tx, float ty);
 
+/**
+ * firtree_affine_transform_get_elements:
+ * @self: An affine transform.
+ * @m11: A pointer to a float to receive the (1,1) element of the transform.
+ * @m12: A pointer to a float to receive the (1,2) element of the transform.
+ * @m21: A pointer to a float to receive the (2,1) element of the transform.
+ * @m22: A pointer to a float to receive the (2,2) element of the transform.
+ * @tx: A pointer to a float to receive the translation along x.
+ * @ty: A pointer to a float to receive the translation along y.
+ *
+ * Retrieve the elements of the transform @self.
+ */
 void
 firtree_affine_transform_get_elements (FirtreeAffineTransform* self, 
         float* m11, float* m12, float* m21, float* m22, float* tx, float* ty);
 
+/**
+ * firtree_affine_transform_set_transform:
+ * @self: An affine transform.
+ * @src: An affine transform.
+ * 
+ * Modify @self so that it is identical to the transform @src.
+ */
 void
 firtree_affine_transform_set_transform (FirtreeAffineTransform* self, 
         FirtreeAffineTransform* src);

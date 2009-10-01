eb6da24f25586f9cc828665bd45a731cac0df259
Rich Wareham <richwareham@gmail.com>
2009-10-01 11:47:08 +0100

    Re-indent FirtreeCairoSampler

--- firtree/firtree-cairo-surface-sampler.h
+++ firtree/firtree-cairo-surface-sampler.h
@@ -17,134 +17,64 @@
  * Franklin Street, Fifth Floor, Boston, MA    02110-1301, USA
  */
 
-#ifndef _FIRTREE_CAIRO_SURFACE_SAMPLER
-#define _FIRTREE_CAIRO_SURFACE_SAMPLER
+#ifndef __FIRTREE_CAIRO_SURFACE_SAMPLER_H__
+#define __FIRTREE_CAIRO_SURFACE_SAMPLER_H__
 
-#include "firtree.h"
+#include <firtree/firtree.h>
 
 #if FIRTREE_HAVE_CAIRO
 
 #include <glib-object.h>
 #include <cairo/cairo.h>
 
-#include "firtree-sampler.h"
-
-/**
- * SECTION:firtree-cairo-surface-sampler
- * @short_description: A FirtreeSampler which can sample from a Cairo image surface.
- * @include: firtree/firtree-cairo-surface-sampler.h
- *
- * A FirtreeCairoSurfaceSampler is a FirtreeSampler which knows how to sample from
- * a Cairo image surface.
- */
+#include <firtree/firtree-sampler.h>
 
 G_BEGIN_DECLS
 
-#define FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER firtree_cairo_surface_sampler_get_type()
+#define FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER 		firtree_cairo_surface_sampler_get_type()
+#define FIRTREE_CAIRO_SURFACE_SAMPLER(obj) 		(G_TYPE_CHECK_INSTANCE_CAST ((obj), FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER, FirtreeCairoSurfaceSampler))
+#define FIRTREE_CAIRO_SURFACE_SAMPLER_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER, FirtreeCairoSurfaceSamplerClass))
+#define FIRTREE_IS_CAIRO_SURFACE_SAMPLER(obj) 		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER))
+#define FIRTREE_IS_CAIRO_SURFACE_SAMPLER_CLASS(klass) 	(G_TYPE_CHECK_CLASS_TYPE ((klass), FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER))
+#define FIRTREE_CAIRO_SURFACE_SAMPLER_GET_CLASS(obj) 	(G_TYPE_INSTANCE_GET_CLASS ((obj), FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER, FirtreeCairoSurfaceSamplerClass))
 
-#define FIRTREE_CAIRO_SURFACE_SAMPLER(obj) \
-    (G_TYPE_CHECK_INSTANCE_CAST ((obj), FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER, FirtreeCairoSurfaceSampler))
+typedef struct _FirtreeCairoSurfaceSampler 		FirtreeCairoSurfaceSampler;
+typedef struct _FirtreeCairoSurfaceSamplerClass 	FirtreeCairoSurfaceSamplerClass;
+typedef struct _FirtreeCairoSurfaceSamplerPrivate 	FirtreeCairoSurfaceSamplerPrivate;
 
-#define FIRTREE_CAIRO_SURFACE_SAMPLER_CLASS(klass) \
-    (G_TYPE_CHECK_CLASS_CAST ((klass), FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER, FirtreeCairoSurfaceSamplerClass))
+struct _FirtreeCairoSurfaceSampler 
+{
+	FirtreeSampler 		parent;
+};
 
-#define FIRTREE_IS_CAIRO_SURFACE_SAMPLER(obj) \
-    (G_TYPE_CHECK_INSTANCE_TYPE ((obj), FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER))
+struct _FirtreeCairoSurfaceSamplerClass 
+{
+	FirtreeSamplerClass parent_class;
+};
 
-#define FIRTREE_IS_CAIRO_SURFACE_SAMPLER_CLASS(klass) \
-    (G_TYPE_CHECK_CLASS_TYPE ((klass), FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER))
+GType 				 firtree_cairo_surface_sampler_get_type	(void);
 
-#define FIRTREE_CAIRO_SURFACE_SAMPLER_GET_CLASS(obj) \
-    (G_TYPE_INSTANCE_GET_CLASS ((obj), FIRTREE_TYPE_CAIRO_SURFACE_SAMPLER, FirtreeCairoSurfaceSamplerClass))
+FirtreeCairoSurfaceSampler 	*firtree_cairo_surface_sampler_new	(void);
 
-/**
- * FirtreeCairoSurfaceSampler:
- * @parent: The parent FirtreeSampler.
- *
- * A structure representing a FirtreeCairoSurfaceSampler object.
- */
-typedef struct {
-    FirtreeSampler parent;
-} FirtreeCairoSurfaceSampler;
+void				 firtree_cairo_surface_sampler_set_cairo_surface
+									(FirtreeCairoSurfaceSampler 	*self,
+									 cairo_surface_t		*cairo_surface);
 
-typedef struct {
-    FirtreeSamplerClass parent_class;
-} FirtreeCairoSurfaceSamplerClass;
+cairo_surface_t			*firtree_cairo_surface_sampler_get_cairo_surface
+									(FirtreeCairoSurfaceSampler   	*self);
 
-GType firtree_cairo_surface_sampler_get_type (void);
+gboolean			 firtree_cairo_surface_sampler_get_do_interpolation
+									(FirtreeCairoSurfaceSampler	*self);
 
-/**
- * firtree_cairo_surface_sampler_new:
- *
- * Construct an uninitialised cairo_surface sampler. Until this has been associated
- * with a cairo_surface via firtree_cairo_surface_sampler_new_set_cairo_surface(), the sampler is
- * invalid.
- *
- * Returns: A new FirtreeCairoSurfaceSampler.
- */
-FirtreeCairoSurfaceSampler* 
-firtree_cairo_surface_sampler_new (void);
-
-/**
- * firtree_cairo_surface_sampler_set_cairo_surface:
- * @self: A FirtreeCairoSurfaceSampler.
- * @cairo_surface: A cairo_surface_t.
- *
- * Set @cairo_surface as the cairo_surface associated with this sampler. Drop any references
- * to any other cairo_surface previously associated. The sampler increments the 
- * reference count of the passed cairo_surface to 'claim' it.
- *
- * Pass NULL in order to desociate this sampler with any cairo_surface.
- */
-void
-firtree_cairo_surface_sampler_set_cairo_surface (FirtreeCairoSurfaceSampler* self,
-        cairo_surface_t* cairo_surface);
-
-/**
- * firtree_cairo_surface_sampler_get_cairo_surface:
- * @self: A FirtreeCairoSurfaceSampler.
- *
- * Retrieve the cairo_surface previously associated with this sampler via
- * firtree_cairo_surface_sampler_set_cairo_surface(). If no cairo_surface is associated,
- * NULL is returned.
- *
- * If the caller wishes to maintain a long-lived reference to the cairo_surface,
- * its reference count should be increased.
- *
- * Returns: The cairo_surface associated with the sampler or NULL if there is none.
- */
-cairo_surface_t*
-firtree_cairo_surface_sampler_get_cairo_surface (FirtreeCairoSurfaceSampler* self);
-
-/**
- * firtree_cairo_surface_sampler_get_do_interpolation:
- * @self:  A FirtreeCairoSurfaceSampler.
- *
- * Get a flag which indicates if the sampler should attempt linear interpolation
- * of the pixel values.
- *
- * Returns: A flag indicating if interpolation is performed.
- */
-gboolean
-firtree_cairo_surface_sampler_get_do_interpolation (FirtreeCairoSurfaceSampler* self);
-
-/**
- * firtree_cairo_surface_sampler_set_do_interpolation:
- * @self:  A FirtreeCairoSurfaceSampler.
- * @do_interpolation: A flag indicating if interpolation is performed.
- *
- * Set a flag which indicates if the sampler should attempt linear interpolation
- * of the pixel values.
- */
-void
-firtree_cairo_surface_sampler_set_do_interpolation (FirtreeCairoSurfaceSampler* self,
-        gboolean do_interpolation);
+void				 firtree_cairo_surface_sampler_set_do_interpolation
+									(FirtreeCairoSurfaceSampler 	*self,
+									 gboolean do_interp);
 
 G_END_DECLS
 
-#endif /* FIRTREE_HAVE_CAIRO */
+#endif				/* FIRTREE_HAVE_CAIRO */
 
-#endif /* _FIRTREE_CAIRO_SURFACE_SAMPLER */
+#endif				/* __FIRTREE_CAIRO_SURFACE_SAMPLER_H__ */
 
-/* vim:sw=4:ts=4:et:cindent
+/* vim:sw=8:ts=8:tw=78:noet:cindent
  */
